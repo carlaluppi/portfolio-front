d91c51ac4055278f8450cc5c79c4ab7ad750cdb3
carlilu <carluppi99@gmail.com>
2023-02-01 11:04:33 +0100

    created components

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css';
+import React from 'react';
+import Router from './router/PublicRoute';
+
+function App() {
+  return (
+    <>
+      <Router/>
+    </>
+
+  );
+}
+
+export default App;
